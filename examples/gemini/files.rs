078dd385af66b8615b92771ceb820e12c1d0fb83
EggerMarc <eggermarc@gmail.com>
2026-02-23 16:54:14 +0100

    fix(example): messages expect content, not &Parts

--- examples/gemini/files.rs
+++ examples/gemini/files.rs
@@ -38,20 +38,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let mut messages = messages::Messages::default();
     let mut system_messages = content::from_system(vec![
         "You are a helpful assistant. Your job is to be as useful as possible.",
-    ])
-    .parts
-    .push(messages::parts::PartEnum::File(File::from_url(
-        "https://www.youtube.com/watch?v=g-ydgmNjReQ",
-        None,
-    )?));
-
+    ]);
+    system_messages
+        .parts
+        .push(messages::parts::PartEnum::File(File::from_url(
+            "https://www.youtube.com/watch?v=g-ydgmNjReQ",
+            None,
+        )?));
     messages.push(system_messages);
-
     loop {
         let mut user_input = String::new();
         println!("User:\t");
         std::io::stdin().read_line(&mut user_input)?;
-        let mut user_message = content::from_user(vec![&user_input]);
+        let user_message = content::from_user(vec![&user_input]);
 
         messages.push(user_message);
 
