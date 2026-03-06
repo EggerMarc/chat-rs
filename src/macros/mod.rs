48c461f0914b57b082906d716620f18eff148075
EggerMarc <eggermarc@gmail.com>
2026-03-06 15:34:13 -0300

    fix: add move to retry_strategy macro

--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -1,7 +1,7 @@
 #[macro_export]
 macro_rules! retry_strategy {
     (|$ctx:ident| $body:block) => {
-        Box::new(|$ctx| {
+        Box::new(move |$ctx| {
             Box::pin(async move $body)
         })
     };
